87883b380866876c293d8d0cd7eeec585ea3efc0
FentPams <feng.xinyi@northeastern.edu>
2022-05-24 18:04:25 -0700

    update main.java

--- CS5004_LAB/LAB03/src/Main.java
+++ CS5004_LAB/LAB03/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world! --From Xinyi");
+    System.out.println("Hello world! What a good day!--From Xinyi");
   }
 }
